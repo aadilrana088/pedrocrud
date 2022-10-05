af9084f4de5dabb8cb2b6d416170057ce363a423
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-10-05 14:12:02 +0530

    Delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { addUser, deleteUser } from './features/Users';
+import { addUser, deleteUser, updateUsername } from './features/Users';
 
 function App() {
     const dispatch = useDispatch();
     const userList = useSelector((state) => state.users.value);
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
+    const [newUsername, setNewUsername] = useState('');
 
     return (
         <div className="App">
@@ -45,10 +46,21 @@ function App() {
                             type="text"
                             placeholder="New Username"
                             onChange={(event) => {
-                                // setName(event.target.value)
+                                setNewUsername(event.target.value);
                             }}
                         />
-                        <button>Update Username</button>
+                        <button
+                            onClick={() =>
+                                dispatch(
+                                    updateUsername({
+                                        id: user.id,
+                                        username: newUsername,
+                                    })
+                                )
+                            }
+                        >
+                            Update Username
+                        </button>
                         <button
                             onClick={() =>
                                 dispatch(deleteUser({ id: user.id }))
