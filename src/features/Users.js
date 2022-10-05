b3d9d40997ce9453530754e8fa609b87a1a6b008
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-10-05 10:54:40 +0530

    Setup Redux environment

---
+++ src/features/Users.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState: {
+        value: [],
+    },
+    reducers: {
+        addUser: (state, action) => {
+            //write code for User
+        },
+    },
+});
